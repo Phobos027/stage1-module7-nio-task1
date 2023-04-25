fc17f1d46141235a4c549e5c76c7b9d1dc0afb2e
Phobos027 <shkuro.stepan@gmail.com>
2023-04-25 22:07:09 +0300

    done

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,40 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 
 public class FileReader {
 
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader();
+        fileReader.getDataFromFile(new File("src/main/resources/Profile.txt"));
+        System.out.println();
+    }
+
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+
+        try {
+            List<String> strings = Files.readAllLines(Paths.get(String.valueOf(file)));
+//            file.close();
+            String[] name = strings.get(0).split(" ");
+            String[] age = strings.get(1).split(" ");
+            String[] eMail = strings.get(2).split(" ");
+            String[] phone = strings.get(3).split(" ");
+
+            profile.setName(name[1]);
+            profile.setAge(Integer.parseInt(age[1]));
+            profile.setEmail(eMail[1]);
+            profile.setPhone(Long.parseLong(phone[1]));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Profile(profile.getName(), profile.getAge(), profile.getEmail(), profile.getPhone());
     }
 }
